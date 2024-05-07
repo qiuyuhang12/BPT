93718c16d9e5191812aa6f118ff9a6176e1f6fbb
grace <2094863012@qq.com>
2024-05-07 19:57:20 +0800

    v 2.14.6 奇葩调参

--- LRU.hpp
+++ LRU.hpp
@@ -26,8 +26,8 @@ template<typename Hash, typename Key>
 concept Hashable = requires(Key key){
     { Hash()(key) }->std::same_as<size_t>;
 };
-static const int TableCapacity = 1e4+7;//!2
-static const int LinkCapacity = 1e4;//!2
+static const int TableCapacity = 25e3+7;//!2
+static const int LinkCapacity = 25e3;//!2
 template<typename Key, typename Block, Hashable<Key> Hash>
 class LRU {
 private:
